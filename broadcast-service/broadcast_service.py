d42c055b41492d7436b6ec9c7719565d7ffe89bc
sahbabos <41399590+sahbabos@users.noreply.github.com>
2024-03-03 01:46:17 -0800

    Revert "[DEV-004] (#4)" (#5)
    This reverts commit 7fdf301228bc45d076cdc56c592a8b5da20bae39.

--- broadcast-service/broadcast_service.py
+++ broadcast-service/broadcast_service.py
@@ -1,62 +1,36 @@
-import os
 import time
 import random
-import socketio
+import requests
 import logging
+import signal
+import sys
 
 # Configure logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Create a Socket.IO client
-sio = socketio.Client()
-
-# Get the receiver service URL from an environment variable (default to 'http://localhost:5002')
-# This is usefull when user trys to use minikube env
-
-RECEIVER_SERVICE_URL = os.getenv(
-    'RECEIVER_SERVICE_URL', 'http://localhost:5002')
-
-
-def connect():
-    logging.info("Connected to Receiver Service")
-
-
-def disconnect():
-    logging.info("Disconnected from Receiver Service")
-
-
-def generate_message():
-    # generate a message
-    return {"message": "Hello world"}
-
-
-def send_message(message):
-    # send the given message to the sio
-    sio.emit('broadcast_message', message)
-    logging.info(f"Message sent: {message['message']}")
-
-
-def get_interval():
-    # get a random number between 1-10
-    return random.randint(1, 10)
-
+# Flag to control the broadcasting loop
+running = True
 
 def broadcast_message():
-    while True:
-        interval = get_interval()
-        message = generate_message()
-        send_message(message)
+    url = "http://localhost:5000/receive"  # URL of the Receiver Service
+    while running:
+        interval = random.randint(1, 10)
+        message = {"message": "Hello world"}
+        try:
+            response = requests.post(url, json=message)
+            if response.status_code == 200:
+                logging.info(f"Message sent: {message['message']}, Status Code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Failed to send message: {e}")
         time.sleep(interval)
 
+def signal_handler(sig, frame):
+    global running
+    running = False
+    logging.info("Shutting down broadcast service...")
 
-if __name__ == '__main__':
-    try:
-        sio.on('connect', connect)
-        sio.on('disconnect', disconnect)
-        sio.connect(RECEIVER_SERVICE_URL)
-        broadcast_message()
-    except Exception as e:
-        logging.error(f"Failed to connect to Receiver Service: {e}")
-    finally:
-        sio.disconnect()
+if __name__ == "__main__":
+    # Register signal handler for graceful shutdown
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+    broadcast_message()
